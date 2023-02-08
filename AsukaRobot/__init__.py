654e10e3e77da9606e7a37ff2cfd2e367ec85805
Yonko9770 <121147878+Yonko9770@users.noreply.github.com>
2023-02-08 12:18:29 +0530

    Update __init__.py

--- AsukaRobot/__init__.py
+++ AsukaRobot/__init__.py
@@ -75,7 +75,7 @@ if ENV:
     INFOPIC = bool(os.environ.get('INFOPIC', False))
     EVENT_LOGS = os.environ.get('EVENT_LOGS', None)
     WEBHOOK = bool(os.environ.get('WEBHOOK', False))
-    ARQ_API_URL = os.environ.get("ARQ_API_URL", None)
+    ARQ_API_URL = os.environ.get("ARQ_API_URL", False)
     ARQ_API_KEY = os.environ.get("ARQ_API_KEY", None)
     URL = os.environ.get('URL', "")  # Does not contain token
     PORT = int(os.environ.get('PORT', 5000))
